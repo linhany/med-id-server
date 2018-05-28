9e4d94ee2c3dadd848f99fd9bb5e84f287a6b0fd
John Yong <lhlhyong@gmail.com>
2018-05-27 18:08:35 -0700

    CORS fix

--- server.js
+++ server.js
@@ -26,6 +26,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/', express.static('public_static'));
 
 // creates a doctor
